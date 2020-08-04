2450611059c3686826ce2a82116ad893e32eb5ff
suresh524 <suresharumulla.a@gmail.com>
2020-08-04 09:14:14 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,12 @@
+console.log(5 === 2 + 4);
+console.log('Chris' === 'Bob');
+console.log(2 === '2');
+console.log(2 === '2');
+
+console.log(5 !== 2 + 4);
+console.log('Chris' !== 'Bob');
+console.log(5 !== 2 + 3);
+console.log(2 !== '2');
+
+console.log(6 < 10);
+console.log(20 < 10);
